1c3cfe2e02d84b34a9d8790b6bc3a49fe447a793
AmitxParmar <amitparmar901@gmail.com>
2022-12-01 23:09:22 +0530

    added header and cards

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Header from "./components/Header";
+import Cards from "./components/Cards";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Cards />
+    </>
+  );
+}
---
+++ src/components/Side.jsx
@@ -0,0 +1,7 @@
+import { Stack } from "@mui/material";
+
+const Side = () => {
+  return <></>;
+};
+
+export default Side;
